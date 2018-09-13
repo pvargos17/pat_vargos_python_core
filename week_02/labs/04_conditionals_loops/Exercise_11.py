82c15d5b8e853c23416801184e20ec57749341a4
Patrikc Vargos <pvargos17@gmail.com>
2018-09-13 10:50:08 +0200

    comepleted problem class

--- week_02/labs/04_conditionals_loops/Exercise_11.py
+++ week_02/labs/04_conditionals_loops/Exercise_11.py
@@ -7,3 +7,15 @@ p.s.: http://www.montypython.net/scripts/dentist.php
 
 secret = "I hear the gooseberries are doing well this year, and so are the mangoes."
 cipher = 7
+
+
+message = input(secret)
+number = input(7)
+final_message = ""
+for char in message:
+    enumerated = ord(char)
+    transformed = enumerated + number
+    new_letter = chr(transformed)
+    final_message += new_letter
+print("Your encoded message is: ", final_message)
+
