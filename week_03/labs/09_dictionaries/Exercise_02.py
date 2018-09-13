060f284c6b612c10326524c9703f8ef5aee4b11e
Patrikc Vargos <pvargos17@gmail.com>
2018-09-13 16:39:09 +0200

    completed during class

--- week_03/labs/09_dictionaries/Exercise_02.py
+++ week_03/labs/09_dictionaries/Exercise_02.py
@@ -10,3 +10,19 @@ dict_2 = {3: 9, 4: 16}
 dict_3 = {5: 25, 6: 36, 7: 49}
 
 
+def combine_dict(a,s,d):
+    new_dict = {}
+    for c in a:
+        new_dict[c] = a[c]
+    for c in s:
+        new_dict[c] = s[c]
+    for c in d:
+        new_dict[c] = d[c]
+    print(new_dict)
+
+    for k, v in new_dict.items():
+        print(k, v)
+
+
+print(combine_dict(dict_1,dict_2,dict_3))
+
