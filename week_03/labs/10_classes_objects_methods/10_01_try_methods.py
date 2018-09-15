c6a666aaaf3dcbcd17361258d8356fae490c13e0
Patrikc Vargos <pvargos17@gmail.com>
2018-09-15 03:05:01 +0200

    wild friday/sat

--- week_03/labs/10_classes_objects_methods/10_01_try_methods.py
+++ week_03/labs/10_classes_objects_methods/10_01_try_methods.py
@@ -48,9 +48,3 @@ print(unknown_job(x))
 
 
 
-# str.find(sub[, start[, end]])
-# Return the lowest index in the string where substring sub is found
-#  within the slice s[start:end]. Optional arguments start and
-#  end are interpreted as in slice notation. Return -1 if sub is not found.
-
-
--- week_03/labs/10_classes_objects_methods/10_03_freeform_classes.py
+++ week_03/labs/10_classes_objects_methods/10_03_freeform_classes.py
@@ -17,3 +17,64 @@ Using objects you can model anything you want.
 Cars, animals, poker games, sports teams, trees, beers, people etc...
 
 '''
+class SportsTeam():
+    """A sports team is a group of individuals who play sports, usually team sports, on the same team
+Attributes:
+            sport (str): Description
+            way_of_winning (str): Description
+            number_of_playes (int) : Description
+    """
+    motto = "Win!"
+    location = "stadium"
+    def __init__(self, sport_name="Chicago Bears", number_of_playes="11", way_of_winning="scoring more!"):
+        self.sport_name = sport_name
+        self.number_of_playes = number_of_playes
+        self.way_of_winning = way_of_winning
+
+    def description(self, numer_of_championships):
+        """
+            Describes the Sports Team with # of championships
+
+            Args:
+            Sport (str): Description
+            way_of_winning (str): Description
+            number_of_playes (int) : Description
+
+            Returns:
+                str: a sentence describing the Sports team
+        """
+        return f"{self.sport_name} have {numer_of_championships} championship(s), & have {self.number_of_playes} players and win games by {self.way_of_winning}"
+
+    def __str__(self, motto, location):
+        return f"{motto} | {location} |{self.sport_name} | {self.number_of_playes} | {self.way_of_winning}"
+
+
+
+#class WhiteSox():
+    """The Chicago White Sox are an American professional baseball team based in Chicago, Illinois
+Attributes:
+            sport (str): Description
+            way_of_winning (str): Description
+            number_of_playes (int) : Description
+    """
+
+
+
+
+
+#class BirminghamBarons():
+    """The Birmingham Barons are a Minor League Baseball team based in Birmingham, Alabama.
+        The team, which plays in the Southern League,is the Double-A affiliate of the Chicago White
+         Sox and plays at Regions Field in downtown Birmingham
+Attributes:
+            sport (str): Description
+            way_of_winning (str): Description
+            number_of_playes (int) : Description
+    """
+
+
+Chi_Bears = SportsTeam("DA BEARS", "11", "being monsters of the midway")
+Chi_Flubs = SportsTeam("The Flubs","no class ", "stealing Theo or else they would go back to sucking!")
+print(Chi_Bears.description(1))
+print(Chi_Flubs.description(2))
+print(Chi_Bears)
