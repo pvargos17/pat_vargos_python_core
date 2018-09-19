57bc8ea2dc9aa62a3ac34751396354afa3f5635f
Patrikc Vargos <pvargos17@gmail.com>
2018-09-19 20:43:03 +0200

    completed

--- week_04/labs/06_generators/Exercise_02.py
+++ week_04/labs/06_generators/Exercise_02.py
@@ -4,3 +4,9 @@ Adapt your Generator expression from the previous Exercise
 What numbers do you get?
 
 '''
+nums = range(1, 1000000)
+
+gen = (n for n in nums if (n % 1111 == 0))
+
+print([n // 1111 for n in gen])
+
